e2a7f28d86f3d5effea14797039272945b8c3fd6
RASBORA <greatest_admiral@yahoo.co.jp>
2017-03-07 20:21:25 +0900

    Fix create_token.py

--- create_token.py
+++ create_token.py
@@ -3,28 +3,25 @@
 
 import tweepy
 import webbrowser
-import time
-import platform
-import json
 
-from pprint import pprint
+CONSUMER_KEY = 'bHvXbznxDz0UhaKz7q1BHetOY'
+CONSUMER_SECRET = 'tC5RNt2d9lLW9PT9M2qVyi8kz28U2fRyVBI3oKfRarodUU0GuC'
 
-CONSUMER_KEY = '7J5zlGbq8ksp7TnPRGVcl0eGk'
-CONSUMER_SECRET = 'XojN3JUsoD3c7N0CBVxvNA7eKk5vJmMpgO7N2muLVZoy4dZkD0'
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 
 redirect_url = auth.get_authorization_url()
-print 'Get your verification code from: ' + redirect_url
+print('Get your verification code from: ' + redirect_url)
 
 webbrowser.open(redirect_url)
-verifier = raw_input('Type the verification code: ').strip()
+verifier = input('Type the verification code: ').strip()
 
 auth.get_access_token(verifier)
 ACCESS_TOKEN = auth.access_token
 ACCESS_SECRET = auth.access_token_secret
 
-ACCESS_TOKEN_HANDLE     = open("ACCESS_TOKEN","w")
-ACCESS_SECRET_HANDLE    = open("ACCESS_SECRET","w")
-ACCESS_TOKEN_HANDLE.write(ACCESS_TOKEN)
-ACCESS_SECRET_HANDLE.write(ACCESS_SECRET)
+with open("ACCESS_TOKEN","w") as fp:
+    fp.write(ACCESS_TOKEN)
+with open("ACCESS_SECRET","w") as fp:
+    fp.write(ACCESS_SECRET)
 
 # http://www.statsbeginner.net/entry/2015/10/21/131717
