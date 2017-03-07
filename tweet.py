0b2dfec28449724b7e1b8dd647b40f1a6a5f9e5f
RASBORA <greatest_admiral@yahoo.co.jp>
2017-03-07 19:34:58 +0900

    Fix bug for ACCESS_TOKEN

--- tweet.py
+++ tweet.py
@@ -7,17 +7,23 @@ import sys
 CONSUMER_KEY = '7J5zlGbq8ksp7TnPRGVcl0eGk'
 CONSUMER_SECRET = 'XojN3JUsoD3c7N0CBVxvNA7eKk5vJmMpgO7N2muLVZoy4dZkD0'
 
+ACCESS_TOKEN = open('ACCESS_TOKEN', 'r').read()
+ACCESS_SECRET = open('ACCESS_SECRET', 'r').read()
+
 TWEET_HEADER = u'' # example: u'@user'
 TWEET_FOOTER = u''
 
+# Authorize
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
 api = tweepy.API(auth)
 
+# Create tweet
 text = u'' + TWEET_HEADER + u'\n'
 for arg in sys.argv:
     text += arg + u'\n'
 
 text += TWEET_FOOTER
 
+# Send tweet
 api.update_status(status=text)
